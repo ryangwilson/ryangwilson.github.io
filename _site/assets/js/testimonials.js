177a66a506aba57bd5d278fb5205f432216ddac4
Ryan G. Wilson <ryanwilson@rgwpersonalmbp.lan>
2022-04-20 22:43:58 -0400

    adds only leadership testimonials at top section of about page

--- _site/assets/js/testimonials.js
+++ _site/assets/js/testimonials.js
@@ -211,11 +211,11 @@ const testimonials = [
 
 function shuffleArray(array) {
   const arrayCopy = array.slice(0);
-  for (var i = array.length - 1; i > 0; i--) {
+  for (var i = arrayCopy.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+      var temp = arrayCopy[i];
+      arrayCopy[i] = arrayCopy[j];
+      arrayCopy[j] = temp;
   }
   return arrayCopy;
 }
